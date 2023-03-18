bd5d8f33a4bb7f2d8b4e09b8789ef4cb39325d9c
Sam Chappel <m.samchappel@gmail.com>
2023-03-17 18:57:52 -0700

    after testing in python repl

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -13,3 +13,6 @@ class GetRequester:
     def load_json(self):
         response_body = self.get_response_body()
         return json.loads(response_body)
+    
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.load_json()
